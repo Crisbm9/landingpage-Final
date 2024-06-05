0912a72909effc4160e7cb087aa36db9981da633
Crisbm9 <cristinab.tandem@gmail.com>
2024-06-05 13:27:57 +0200

    info

--- src/pages/crear-qr.js
+++ src/pages/crear-qr.js
@@ -1,62 +1,3 @@
-// import * as React from "react";
-// import { useState } from "react";
-// import Layout from "../components/layout";
-// import Collapse2 from "../components/crearqr-componentes/collapse2";
-// import MyComponent from "../components/crearqr-componentes/componente1";
-// import TandemButton from "../components/button/tandembutton";
-// import QRCode from 'qrcode.react';
-// import "../components/layout.css"
-// import { toPng } from 'html-to-image';
-
-// function Crearqr() {
-//   const [inputValue, setInputValue] = useState('');
-//   const [qrColor, setQrColor] = useState('black');
-//   const [qrSize, setQrSize] = useState(100);
-//   const handleInputChange = (event) => {
-//     setInputValue(event.target.value);
-//   };
-//   const handleColorChange = (color) => {
-//     setQrColor(color);
-//   };
-//   const handleSizeChange = (size) => {
-//     setQrSize(parseInt(size, 10));
-//   };
-//   const containerStyle = {
-//     backgroundColor: "beige",
-//     border: "2px solid black",
-//     padding: "20px",
-//     borderRadius: "10px",
-//     textAlign: "center",
-//   };
-
-  
-
-//   return (
-//     <Layout>
-//       <div style={containerStyle}>
-//         <h1>GENERADOR DE QR</h1>
-//         <Collapse2 />
-//         <br />
-//         <p>Introduce tu texto o url aquí:</p>
-//         <input type="text" value={inputValue} onChange={handleInputChange} />
-//         <MyComponent onColorChange={handleColorChange} onSizeChange={handleSizeChange} />
-//         <br />
-//         <div className="qr-contenido">
-       
-//         <QRCode value={inputValue} size={qrSize} fgColor={qrColor} />
-//         <br />
-//         {/* <TandemButton enlace="#">Generar QR</TandemButton> */}
-//         <p>Contenido del QR:</p>
-//         <p>{inputValue}</p>
-//         </div>
-//       </div>
-//     </Layout>
-//   );
-// }
-// export default Crearqr;
-
-// ///////////////////////////////////////////////////////
-
 import * as React from "react";
 import { useState, useRef } from "react";
 import Layout from "../components/layout";
@@ -66,6 +7,7 @@ import TandemButton from "../components/button/tandembutton";
 import QRCode from 'qrcode.react';
 import { toPng } from 'html-to-image';
 import download from 'downloadjs';
+
 function Crearqr() {
   const [inputValue, setInputValue] = useState('');
   const [qrColor, setQrColor] = useState('black');
