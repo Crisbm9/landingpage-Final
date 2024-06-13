8fdceb596fcec6f1edc613214e4d0517d68bde3e
Crisbm9 <cristinab.tandem@gmail.com>
2024-06-13 09:07:55 +0200

    mapa problemas

--- src/components/mapa.js
+++ src/components/mapa.js
@@ -1,31 +1,34 @@
-import React, { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+import React from 'react';
+import { MapContainer, TileLayer, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-
-// Arreglar el problema de los iconos que no se muestran
+// Importar los íconos
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-    iconUrl,
-    iconRetinaUrl,
-    shadowUrl,
-});
-
+// Configurar los íconos de Leaflet
+if (L && L.Icon && L.Icon.Default) {
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+        iconUrl,
+        iconRetinaUrl,
+        shadowUrl,
+    });
+} else {
+    console.error("L o L.Icon.Default no está definido.");
+}
 const LocationMarker = ({ setLatLng }) => {
     useMapEvents({
         click(e) {
-            setLatLng(e.latlng);
+            if (e && e.latlng) {
+                setLatLng(e.latlng);
+            } else {
+                console.error("El evento o la latitud/longitud no están definidos.");
+            }
         },
     });
-
     return null;
 };
-
 const MapaConMarcador = ({ setLatLng }) => {
     return (
         <MapContainer center={[40.030501, -3.604052]} zoom={13} style={{ height: '50vh', width: '100%' }}>
@@ -37,6 +40,48 @@ const MapaConMarcador = ({ setLatLng }) => {
         </MapContainer>
     );
 };
-
-
 export default MapaConMarcador;
+
+
+// import React, { useState } from 'react';
+// import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
+// import 'leaflet/dist/leaflet.css';
+// import L from 'leaflet';
+
+// // Arreglar el problema de los iconos que no se muestran
+// import iconUrl from 'leaflet/dist/images/marker-icon.png';
+// import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+// import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+// delete L.Icon.Default.prototype._getIconUrl;
+
+// L.Icon.Default.mergeOptions({
+//     iconUrl,
+//     iconRetinaUrl,
+//     shadowUrl,
+// });
+
+// const LocationMarker = ({ setLatLng }) => {
+//     useMapEvents({
+//         click(e) {
+//             setLatLng(e.latlng);
+//         },
+//     });
+
+//     return null;
+// };
+
+// const MapaConMarcador = ({ setLatLng }) => {
+//     return (
+//         <MapContainer center={[40.030501, -3.604052]} zoom={13} style={{ height: '50vh', width: '100%' }}>
+//             <TileLayer
+//                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//             />
+//             <LocationMarker setLatLng={setLatLng} />
+//         </MapContainer>
+//     );
+// };
+
+
+// export default MapaConMarcador;
